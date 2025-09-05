cace5240156230a2c91df13283f4c32228f325fd
Nikki Yodo <nik_yodo@yahoo.com>
2025-09-05 10:08:00 -0500

    add umami analytics

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+useHead({
+  title: 'Nikki no Nikki',
+  meta: [
+    { name: 'description', content: 'Nikki\' diary.' }
+  ],
+  script: [ 
+    { 
+      defer: true,
+      src: 'https://cloud.umami.is/script.js', 
+      'data-website-id': '4188cb71-1e7f-48dc-9d95-1e4943024dcf'
+    }
+  ]
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
