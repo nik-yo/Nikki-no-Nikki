ae012f993d339161d1e198f3823a252e60d0ecc9
Nikki Yodo <nik_yodo@yahoo.com>
2025-12-11 14:13:35 -0600

    add date and sort posts

--- src/pages/posts.vue
+++ src/pages/posts.vue
@@ -14,8 +14,8 @@ const { data } = await useAsyncData('navigation', () => {
           <li v-for="month in year.children" :key="month.path" class="ml-4">
             {{ year.title }}/{{ month.title }}
             <ul class="list-disc ml-2">
-              <li v-for="post in month.children" :key="post.path" class="ml-4">
-                <NuxtLink :to="post.path" class="text-blue-600">{{ post.title.substring(3) }}</NuxtLink>
+              <li v-for="post in month.children?.sort((a,b) => parseInt(b.title.substring(0,2)) - parseInt(a.title.substring(0,2)))" :key="post.path" class="ml-4">
+                {{ post.title.substring(0,2) }}: <NuxtLink :to="post.path" class="text-blue-600">{{ post.title.substring(3) }}</NuxtLink>
               </li>
             </ul>
           </li>
